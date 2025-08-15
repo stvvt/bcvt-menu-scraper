c9702674e1498f542bbe213d53cd83a3e9f643c8
Stefan Stefanov <stefan.stefanov@jakala.com>
2025-08-15 18:25:20 +0300

    Fix issue when meal name contain double quotes

--- categorize.sh
+++ categorize.sh
@@ -6,7 +6,8 @@ test ! -f db/categories.json && echo '{}' > db/categories.json
 jq -r --slurpfile existing db/categories.json '
   map(select(.name | in($existing[0]) | not)) | .[].name
 ' db/merged.json \
-  | xargs -I {} node categorize.js "{}" \
+  | tr '\n' '\0' \
+  | xargs -0 -I {} node categorize.js {} \
   | jq -s 'add' \
   | jq 'input + .' - db/categories.json > db/categories_new.json \
   && mv db/categories_new.json db/categories.json
