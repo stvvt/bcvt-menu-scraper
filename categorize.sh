fa79db416cb87583de10ccfe7e84a2300baf613d
Stefan Stefanov <stefan.stefanov@jakala.com>
2026-02-20 18:54:15 +0200

    Parametrize categorization

--- categorize.sh
+++ categorize.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 
-# ensure the file exists
-test ! -f db/bcvt/categories.json && echo '{}' > db/bcvt/categories.json
+venue=${1:?Usage: $0 <venue>}
 
-jq -r --slurpfile existing db/bcvt/categories.json '
+# ensure the file exists
+test ! -f db/${venue}/categories.json && echo '{}' > db/${venue}/categories.json
+
+jq -r --slurpfile existing db/${venue}/categories.json '
   map(select(.name | in($existing[0]) | not)) | .[].name
-' db/bcvt/merged.json \
+' db/${venue}/merged.json \
   | tr '\n' '\0' \
   | xargs -0 -I {} node categorize.mjs {} \
   | jq -s 'add' \
-  | jq 'input + .' - db/bcvt/categories.json > db/bcvt/categories_new.json \
-  && mv db/bcvt/categories_new.json db/bcvt/categories.json
+  | jq 'input + .' - db/${venue}/categories.json > db/${venue}/categories_new.json \
+  && mv db/${venue}/categories_new.json db/${venue}/categories.json
--- categorize_rich.sh
+++ categorize_rich.sh
@@ -1,16 +1,18 @@
 #!/bin/bash
 
+venue=${1:?Usage: $0 <venue>}
+
 CATEGORIES_FILE_NAME="categories_rich"
 CATEGORIZE_SCRIPT="categorize_rich.mjs"
 
 # ensure the file exists
-test ! -f db/bcvt/${CATEGORIES_FILE_NAME}.json && echo '{}' > db/bcvt/${CATEGORIES_FILE_NAME}.json
+test ! -f db/${venue}/${CATEGORIES_FILE_NAME}.json && echo '{}' > db/${venue}/${CATEGORIES_FILE_NAME}.json
 
-jq -r --slurpfile existing db/bcvt/${CATEGORIES_FILE_NAME}.json '
+jq -r --slurpfile existing db/${venue}/${CATEGORIES_FILE_NAME}.json '
   map(select(.name | in($existing[0]) | not)) | .[].name
-' db/bcvt/merged.json \
+' db/${venue}/merged.json \
   | tr '\n' '\0' \
   | xargs -0 -I {} node ${CATEGORIZE_SCRIPT} {} \
   | jq -s 'add' \
-  | jq 'input + .' - db/bcvt/${CATEGORIES_FILE_NAME}.json > db/bcvt/${CATEGORIES_FILE_NAME}_new.json \
-  && mv db/bcvt/${CATEGORIES_FILE_NAME}_new.json db/bcvt/${CATEGORIES_FILE_NAME}.json
+  | jq 'input + .' - db/${venue}/${CATEGORIES_FILE_NAME}.json > db/${venue}/${CATEGORIES_FILE_NAME}_new.json \
+  && mv db/${venue}/${CATEGORIES_FILE_NAME}_new.json db/${venue}/${CATEGORIES_FILE_NAME}.json
