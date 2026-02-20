6f6f132970a337f58d2db65bb50356d0b8d63e2f
Stefan Stefanov <stefan.stefanov@jakala.com>
2026-02-20 18:40:37 +0200

    Reorganize db folder - add new nesting level with venue name on top

--- scrape.sh
+++ scrape.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 # scrape the menu from the website using compiled JS
-node dist/cli.js menu https://bcvt.eu/L/S/21254/m/Dirkhlbn --pretty > db/daily/$(date +%Y-%m-%d).json
+node dist/cli.js menu https://bcvt.eu/L/S/21254/m/Dirkhlbn --pretty > db/bcvt/daily/$(date +%Y-%m-%d).json
 
-./merge.sh db/daily/*.json > db/merged.json
+./merge.sh db/bcvt/daily/*.json > db/bcvt/merged.json
 
 ./categorize.sh
 ./categorize_rich.sh
